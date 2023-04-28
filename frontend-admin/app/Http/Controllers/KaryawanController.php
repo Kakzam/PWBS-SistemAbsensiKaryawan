c735a761bac7e42af597715892d05ee85d2232d2
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-04-28 22:04:48 +0700

    membuat function index di controller karyawan untuk mengambil data karyawan dan jabatan di server

--- frontend-admin/app/Http/Controllers/KaryawanController.php
+++ frontend-admin/app/Http/Controllers/KaryawanController.php
@@ -11,4 +11,24 @@ class KaryawanController extends Controller
         $this->client = new \GuzzleHttp\Client();
 
     }
+
+    function index()
+    {
+        $url_karyawan = env("URL_SERVER") . "/api/karyawan";
+        $url_jabatan = env("URL_SERVER") . "/api/jabatan";
+
+        $request_jabatan = $this->client->get($url_jabatan);
+        $request_karyawan = $this->client->get($url_karyawan);
+
+        $response_jabatan = $request_jabatan->getBody();
+        $response_karyawan = $request_karyawan->getBody();
+
+        $data = [
+            'url' => env("URL_SERVER"),
+            'result_karyawan' => json_decode($response_karyawan),
+            'result_jabatan' => json_decode($response_jabatan)
+        ];
+
+        return view("karyawan/vw_karyawan",$data);
+    }
 }
