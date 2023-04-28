3b1f50b662b16b03688cb8b2d565137c5cef0ee6
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-04-28 13:57:01 +0700

    membuat function create karyawan di karyawan controller untuk menambahkan karyawan

--- backend/app/Http/Controllers/KaryawanController.php
+++ backend/app/Http/Controllers/KaryawanController.php
@@ -11,4 +11,21 @@ class KaryawanController extends Controller
     {
         $this->model = new Mkaryawan();
     }
+
+    public function create_karyawan(Request $request)
+    {
+        $karyawan = $this->model->create([
+            "jabatan_id" => $request->jabatan_id,
+            "nik" => $request->nik,
+            "nama" => $request->nama,
+            "username" => $request->username,
+            "password" => $request->password
+        ]);
+
+        return response([
+            "message" => "Data created successfully",
+            "param" => true,
+            "karyawan" => $karyawan
+        ], 201);
+    }
 }
