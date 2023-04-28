82ff65b2089f53972e67670a2d0d99e79352a361
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-04-28 14:15:27 +0700

    membuat form input jabatan di view jabatan

--- frontend-admin/resources/views/jabatan/vw_jabatan.blade.php
+++ frontend-admin/resources/views/jabatan/vw_jabatan.blade.php
@@ -405,6 +405,21 @@
 </head>
 
 <body class="antialiased">
+    <form>
+        {{-- <form action="{{ env('URL_SERVER') . '/jabatan' }}" method="post"> --}}
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Nama Jabatan</label>
+            <input type="text" class="form-control" id="nama_jabatan">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Gaji</label>
+            <input type="number" class="form-control" id="gaji">
+        </div>
+        <button type="button" class="btn btn-primary" onclick="simpanData()">Submit</button>
+    </form>
+
+    <br>
+    <br>
 </body>
 
 </html>
