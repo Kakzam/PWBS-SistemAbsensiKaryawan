d064147fa36bc36b5cff3cd3eddd5db31ef2d119
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-04-28 13:51:10 +0700

    membuat migrasi struktur database model karyawan

---
+++ backend/database/migrations/2023_04_27_033241_create_mkaryawans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMkaryawansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('jabatan_id');
+            $table->string("nik")->unique();
+            $table->string("nama");
+            $table->string("username")->unique();
+            $table->string("password");
+            $table->string("delete")->default('1');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('m_karyawan');
+    }
+}
