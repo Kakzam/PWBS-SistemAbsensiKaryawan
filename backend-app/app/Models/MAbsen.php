5efb1967e127efab92d0d4b7a146c04d73ae2326
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-05-29 13:26:11 +0700

    membuat protected $fillable di MAbsen

--- backend-app/app/Models/MAbsen.php
+++ backend-app/app/Models/MAbsen.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class MAbsen extends Model
 {
     use HasFactory;
+    protected $fillable = ['id_user', 'id_jabatan', 'gaji', 'tangal', 'bulan', 'tahun'];
 }
