73f4aa6af97b45af6ce89476817cf6657b0bd4b8
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-05-29 13:26:19 +0700

    membuat protected $fillable di MGaji

--- backend-app/app/Models/MGaji.php
+++ backend-app/app/Models/MGaji.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class MGaji extends Model
 {
     use HasFactory;
+    protected $fillable = ['id_user', 'id_jabatan', 'gaji', 'tangal', 'bulan', 'tahun', 'total_absen', 'total_gaji'];
 }
