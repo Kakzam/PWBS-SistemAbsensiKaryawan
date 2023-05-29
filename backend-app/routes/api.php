9de8354f3090011edeef3dda4f0aaeaa24f0fb03
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-05-29 14:26:46 +0700

    membuat jalur api dari controller user dan jabatan

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerAbsen;
+use App\Http\Controllers\ControllerGaji;
+use App\Http\Controllers\ControllerJabatan;
+use App\Http\Controllers\ControllerUser;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/* Validasi Data Login */
+Route::post('login',[ControllerUser::class,'verifikasi']);
+
+/* Menambahkan Data User */
+Route::post('user',[ControllerUser::class,'createdData']);
+
+/* Mengambil Semua Data User */
+Route::get('user',[ControllerUser::class,'getData']);
+
+/* Menghapus Data User Berdasarkan ID */
+Route::delete('user/{parameter}',[ControllerUser::class,'deleteData']);
+
+/* Mengupdate Data User */
+Route::put('user',[ControllerUser::class,'updateData']);
+
+/*************************************************************************/
+
+/* Menambahkan Data Jabatan */
+Route::post('user',[ControllerJabatan::class,'createdData']);
+
+/* Mengambil Semua Data Jabatan */
+Route::get('user',[ControllerJabatan::class,'getData']);
+
+/* Menghapus Data Jabatan Berdasarkan ID */
+Route::delete('user/{parameter}',[ControllerJabatan::class,'deleteData']);
+
+/* Mengupdate Data Jabatan */
+Route::put('user',[ControllerJabatan::class,'updateData']);
+
+/*************************************************************************/
