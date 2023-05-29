1325876a88441d0a532965b2ccd90610da485406
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-05-29 13:50:49 +0700

    fix controller user

--- backend-app/app/Http/Controllers/ControllerUser.php
+++ backend-app/app/Http/Controllers/ControllerUser.php
@@ -3,8 +3,98 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MUser;
 
 class ControllerUser extends Controller
 {
-    
+    function __construct()
+    {
+        $this->model = new MUser();
+    }
+
+    public function verifikasi(Request $input)
+    {
+        $username = $input->username;
+        $password = $input->password;
+
+        $login = $this->model->where('username', $username)->first();
+
+        if ($login && ($password == $login->password)) {
+            return response([
+                "message" => "Anda Berhasil Login",
+                "action" => true,
+                "data" => $login
+            ], 200);
+        } else {
+            return response([
+                "message" => "Anda Berhasil Login",
+                "action" => false
+            ], 200);
+        }
+    }
+
+    function getData()
+    {
+        return response([
+            "data" => $this->model->all()
+        ],http_response_code());
+    }
+
+    function delete($parameter)
+    {
+        $user = $this->model->find($parameter);
+
+        if (!$jabatan) {
+            return response([
+                "action" => false,
+                "message" => "Data User Tidak ditemukan"
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response([
+            "message" => "Data User Berhasil Dihapus",
+            "action" => true
+        ], 200);
+    }
+
+    function insert(Request $input)
+    {
+        $this->model->create([
+            'nama_user' => $input->name, 
+            'id_jabatan' => $input->id,
+            'username' => $input->username, 
+            'password' => $input->password
+        ]);
+
+        return response([
+            "action" => true,
+            "message" => "Data User Berhasil Di Input",
+        ],http_response_code());
+    }
+
+    function update(Request $input)
+    {
+        $user = $this->model->where('id', $input->id)->first();
+
+        if(!$user) {
+            return response([
+                "message" => "Data User Gagal Di Update ! (User Tidak Ditemukan ! )",
+                "action" => true
+            ], 201);
+        }
+        
+        $user->update([
+            'nama_user' => $input->name,
+            'id_jabatan' => $input->id,
+            'username' => $input->username,
+            'password' => $input->password
+        ]);
+
+        return response([
+            "action" => true,
+            "message" => "Data User Berhasil Di Update"
+        ],http_response_code());
+    }
 }
