2374d7a824c50406c0abb8597f363d3653e694a3
Muhammad Zam Zam Al Murshalat Adzan <adzan.zam@gmail.com>
2023-05-29 13:50:37 +0700

    fix controller jabatan

--- backend-app/app/Http/Controllers/ControllerJabatan.php
+++ backend-app/app/Http/Controllers/ControllerJabatan.php
@@ -3,8 +3,74 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MJabatan;
 
 class ControllerJabatan extends Controller
 {
-    //
+    function __construct()
+    {
+        $this->model = new MJabatan();
+    }
+
+    function createData(Request $input)
+    {
+        $this->model->create([
+            'nama_jabatan' => $input->name, 
+            'gaji' => $input->gaji
+        ]);
+
+        return response([
+            "action" => true,
+            "message" => "Data Jabatan Berhasil Di Input",
+        ],http_response_code());
+    }
+
+    function getData()
+    {
+        return response([
+            "data" => $this->model->all()
+        ],http_response_code());
+    }
+
+    function deleteData($parameter)
+    {
+        $data = $this->model->find($parameter);
+
+        if (!$data) {
+            return response([
+                "action" => false,
+                "message" => "Data Jabatan Tidak Ditemukan"
+            ], 404);
+        }
+
+        $data->delete();
+
+        return response([
+            "message" => "Data Jabatan Berhasil Dihapus",
+            "action" => true
+        ], 200);
+    }
+
+    function updateData(Request $input)
+    {
+        $data = $this->model->where('id', $input->id)->first();
+
+        if(!$data) {
+            return response([
+                "message" => "Data Jabatan Gagal Di Update ! (Tidak Ditemukan ! )",
+                "action" => true
+            ], 201);
+        }
+        
+        // Ubah data
+        $data->update([
+            'nama_jabatan' => $input->name,
+            'gaji' => $input->gaji
+        ]);
+
+        return response([
+            "action" => true,
+            "message" => "Data Jabatan Berhasil Di Update"
+        ],http_response_code());
+    }
 }
